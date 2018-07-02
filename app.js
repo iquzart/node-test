3677c61656028946925a56a2c964f470749b833c
Muhammed Iqbal <iquzart@hotmail.com>
2018-07-02 13:34:56 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
-});
+const http = require('http');
+const port = 8081;
+const ip = '0.0.0.0';
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World');
+}).listen(port, ip);
+
+console.log(`server is running on ${ip}:${port}`);
